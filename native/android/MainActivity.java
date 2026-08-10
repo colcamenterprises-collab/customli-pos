0ac5e9959a2ac288b2b84465c9a576a05932954a
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-08-10 22:31:01 +0700

    Add Customli POS native Android bridge registration

---
+++ native/android/MainActivity.java
@@ -0,0 +1,12 @@
+package io.customli.pos;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(ThermalPrinterPlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
