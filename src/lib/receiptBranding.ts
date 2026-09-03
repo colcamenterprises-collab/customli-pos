eb0d5de7d7f4bb53b448352e9f0b36ec1ada27d4
colcamenterprises-collab <colcamenterprises@gmail.com>
2026-09-03 20:04:02 +0700

    feat: add receipt branding constants

---
+++ src/lib/receiptBranding.ts
@@ -0,0 +1,9 @@
+export const SBB_RECEIPT_WEBSITE = "SMASHBROSBURGERS.COM";
+export const SBB_MEMBERSHIP_QR_URL = "https://smashbrosburgers.com/membership";
+
+/**
+ * The final SBB monochrome raster logo will be wired here when supplied.
+ * Keeping the branding constants separate means the receipt formatter does not
+ * need to change when the membership URL or logo asset changes.
+ */
+export const SBB_RECEIPT_BRAND_NAME = "SMASH BROTHERS BURGERS";
